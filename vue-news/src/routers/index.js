a12fee5ce0d43744d3bdb64707b7425d20e93ece
youjaewoong <nnv711@gmail.com>
2021-09-22 12:14:22 +0900

    Revert "동적 라우트 매칭 원리 및 적용 '/user/:id'"
    This reverts commit 4a13310516795eeb5ccf15a68cd8b4e736c6ff9c.

--- vue-news/src/routers/index.js
+++ vue-news/src/routers/index.js
@@ -29,7 +29,7 @@ export const router = new VueRouter({
         component: JobsView,
       },
       {
-        path: '/user/:id',
+        path: '/user',
         component: UserView,
       },
       {
--- vue-news/src/views/NewsView.vue
+++ vue-news/src/views/NewsView.vue
@@ -4,11 +4,7 @@
       <a v-bind:href="item.url" target="_blank">
         {{ item.title }}
       </a>
-      <small> 
-        <router-link v-bind:to="`/user/${item.user}`"> 
-          {{ item.time_ago }} by {{ item.user }}
-        </router-link>
-      </small>
+      <small> {{ item.time_ago }} by {{ item.user }} </small>
     </p>
   </div>
 </template>
