108b3eeb9f671de4ec6e345b6b50023613dc7b19
youjaewoong <nnv711@gmail.com>
2021-09-29 17:32:43 +0900

    라우터 네비게이션을 이용한 데이터 호출 방법

--- vue-news/src/mixins/ListMixin.js
+++ vue-news/src/mixins/ListMixin.js
@@ -1,15 +1,18 @@
 import bus from '../utils/bus.js';
 export default {
-    //재사용할 컴포넌트 옵션
-    created() {
-        bus.$emit('start:spinner');
-        this.$store.dispatch('FETCH_LIST', this.$route.name)
-        .then(() => {
-            console.log('fetched');
-            bus.$emit('end:spinner');
-        })
-        .catch((error) => {
-            console.log(error);
-        });
+    //재사용할 컴포넌트 옵션 & 로직
+    mounted() {
+        bus.$emit('end:spinner');
     },
+    // created() {
+    //     bus.$emit('start:spinner');
+    //     this.$store.dispatch('FETCH_LIST', this.$store.name)
+    //     .then(() => {
+    //         console.log('fetched');
+    //         bus.$emit('end:spinner');
+    //     })
+    //     .catch((error) => {
+    //         console.log(error);
+    //     });
+    // }
 }
--- vue-news/src/routers/index.js
+++ vue-news/src/routers/index.js
@@ -1,11 +1,13 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import NewsView from '../views/NewsView.vue'
-//import AskView from '../views/AskView.vue'
+import AskView from '../views/AskView.vue'
 import JobsView from '../views/JobsView.vue'
 import UserView from '../views/UserView.vue'
 import ItemView from '../views/ItemView.vue'
-import createListView from '../views/CreateListView.js'
+//import createListView from '../views/CreateListView.js'
+import bus from '../utils/bus.js';
+import { store } from '../store/index.js'
 
 Vue.use(VueRouter);
 export const router = new VueRouter({ 
@@ -19,19 +21,51 @@ export const router = new VueRouter({
       {
        // path: url 주소
         path: '/news',
-        // component: url 주소로 갔을 때 표시될 컴포넌트
         component: NewsView,
-        name: 'news'
+        name: 'news',
+        beforeEnter: (to, from, next ) => {
+          bus.$emit('start:spinner');
+          store.dispatch('FETCH_LIST', to.name)
+          .then(() => {
+              console.log('fetched');
+              next();
+          })
+          .catch((error) => {
+              console.log(error);
+          });
+         },
       },
       {
         path: '/ask',
-        component: createListView('AskView'),
-        name: 'ask'
+        component: AskView,
+        name: 'ask',
+        beforeEnter: (to, from, next ) => {
+          bus.$emit('start:spinner');
+          store.dispatch('FETCH_LIST', to.name)
+          .then(() => {
+              console.log('fetched');
+              next();
+          })
+          .catch((error) => {
+              console.log(error);
+          });
+         },
       },
       {
         path: '/jobs',
         component: JobsView,
-        name: 'jobs'
+        name: 'jobs',
+        beforeEnter: (to, from, next ) => {
+          bus.$emit('start:spinner');
+          store.dispatch('FETCH_LIST', to.name)
+          .then(() => {
+              console.log('fetched');
+              next();
+          })
+          .catch((error) => {
+              console.log(error);
+          });
+         },
       },
       {
         path: '/user/:name',
