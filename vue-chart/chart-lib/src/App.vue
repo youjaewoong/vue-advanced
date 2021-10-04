07132a718273fc6d93eba664ead81c3d41ca6226
gos1004 <nnv711@gmail.com>
2021-10-04 23:21:30 +0900

    결합력 높은 차트 컴포넌트 모듈 제작 방법

--- vue-chart/chart-lib/src/App.vue
+++ vue-chart/chart-lib/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>Chart.js</h1>
-    <bar-chart></bar-chart>
+    <bar-chart v-on:refresh:chart="refreshChart" v-bind:propsdata="chartDataSet"></bar-chart>
     <line-chart></line-chart>
   </div>
 </template>
@@ -15,6 +15,41 @@ export default {
   components: {
     BarChart,
     LineChart
+  },
+  data(){
+    return {
+      chartDataSet: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+      }],
+    }
+  },
+  methods :{
+    refreshChart(){
+      this.chartDataSet = [10, 20];
+    }
+  },
+  created() {
+    // axios.get('charts/line/1')
+    //   .then(response => this.chartDataSet = response.data)
+    //   .catch(error => console.log(error));
   }
 };
 </script>
