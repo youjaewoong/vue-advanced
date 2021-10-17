b83b719525d795576dae5d6836c40485d0cd3cbb
gos1004 <nnv711@gmail.com>
2021-10-17 14:41:10 +0900

    Controlled Component - Input 태그등을 컴포넌트화 처리할 떄 사용한다. - 상위에서 데이터 관리를 한다.

--- design/controlled/src/App.vue
+++ design/controlled/src/App.vue
@@ -1,13 +1,17 @@
 <template>
-  <check-box></check-box>
+  <check-box v-model="checked"></check-box>
 </template>
 
 <script>
 import CheckBox from './components/CheckBox.vue';
-
 export default {
   components: {
     CheckBox
+  },
+  data(){
+    return {
+      checked: true,
+    }
   },
 }
 </script>
